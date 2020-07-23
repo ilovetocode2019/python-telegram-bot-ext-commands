5885f74696a74e01d5ebae9578a05fde389880c4
Nathan <nathanwj123@outlook.com>
2020-07-23 08:25:28 -0700

    Fix a lot of bugs and make cogs better

--- telegram/ext/commands/bot.py
+++ telegram/ext/commands/bot.py
@@ -20,6 +20,8 @@ class Bot:
 
         self.cogs_dict = {}
 
+        self.extensions = {}
+
         self.help_command = help_command
         self.add_command(help_command, name="help", description="Help for the bot", usage="<command|category>")
 
@@ -74,15 +76,6 @@ class Bot:
         self.commands_dict[name] = command
 
         return command
-    def remove_command(self, name):
-        """Removes a command"""
-
-        if name not in self.commands:
-            raise NotFound("No command by that name")
-
-        self.dispatcher.remove_handler(self._handlers[name])
-        self._handlers.pop(name)
-        self.commands_dict.pop(name)
 
     def add_command(self, func, **kwargs):
         """Adds a function as a command"""
@@ -101,6 +94,21 @@ class Bot:
         self._handlers[name] = handler
         self.commands_dict[name] = command
 
+    def remove_command(self, name):
+        """Removes a command"""
+
+        if name not in [x.name for x in self.commands]:
+            raise NotFound("No command by that name")
+
+        command = self.commands_dict[name]
+
+        if command.cog:
+            command.cog.commands.remove(command)
+
+        self.dispatcher.remove_handler(self._handlers[name])
+        self._handlers.pop(name)
+        self.commands_dict.pop(name)
+
     @property
     def commands(self):
         """A list of commands in the bot"""
@@ -116,17 +124,26 @@ class Bot:
             raise LoadError("No setup function")
 
         cog.setup(self)
+        
+    def unload_extension(self, location):
+        """Unloads an extension from the bot"""
 
-    def remove_cog(self, location):
-        """Removes and extension from the bot"""
+        cog_name = self.extensions.get(location)
+        if not cog_name:
+            raise NotFound("Extension is not loaded")
 
-        if location not in self.cogs_dict:
-            raise NotFound("Cog is not loaded")
+        self.remove_cog(cog_name)
 
-        for command in self.cogs_dict[location].commands:
-            self.remove_command(command.name)
+        self.extensions.pop(location)
+
+    def reload_extension(self, location):
+        """Reloads an extension in the bot"""
+
+        pass
 
     def add_cog(self, cog):
+        """Adds a cog to the bot"""
+        
         if not issubclass(cog.__class__, Cog):
             raise LoadError("Cogs must be a subclass of Cog")
 
@@ -137,8 +154,16 @@ class Bot:
                 cog_commands.append(command)
                 command.bot = self
                 command.cog = cog
-                self.add_command(command.func, name=command.name, description=command.description, usage=command.usage, hidden=command.hidden, cog=command.cog, bot=command.bot)
 
+                if command.name in self.commands_dict:
+                    raise CommandAlreadyExists("A command with that name already exists")
+
+                handler = CommandHandler(command.name, command.invoke)
+                self.dispatcher.add_handler(handler)
+
+                self._handlers[command.name] = handler
+                self.commands_dict[command.name] = command
+                
         if hasattr(cog, "cog_check"):
             if not inspect.ismethod(cog.cog_check):
                 raise LoadError("Cog check is not a function")
@@ -152,6 +177,24 @@ class Bot:
         cog.name = cog.__class__.__name__
         cog.commands = cog_commands
         self.cogs_dict[cog.name] = cog
+
+        if str(cog.__module__) != "__main__":
+            self.extensions[cog.__module__] = cog.name
+
+    def remove_cog(self, cog):
+        """Removes and cog from the bot"""
+
+        if cog not in self.cogs_dict:
+            raise NotFound("Cog is not loaded")
+
+        for command in self.cogs_dict[cog].commands:
+            try:
+                self.remove_command(command.name)
+            except NotFound:
+                pass
+
+        self.cogs_dict.pop(cog)
+
     @property
     def cogs(self):
         return list(self.cogs_dict.values())
